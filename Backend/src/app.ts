8ad0392b88b1a6de545ec220e04f6edb09eb54a1
Arif2312323 <arifalmas3.14@gmail.com>
2026-04-18 11:53:44 +0530

    Deployment bug fix

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173"],
+    origin: ["https://bms-frontend-rr67.onrender.com"],
   })
 );
 app.use(cookieParser());
